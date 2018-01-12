8f538f78aa25a72a549df790fbb12972889cd5a6
Patipon Taweechat <57050269@kmitl.ac.th>
2018-01-12 16:52:37 +0700

    Add RelayTest

--- Test/ServoTest/ServoTest.ino
+++ Test/ServoTest/ServoTest.ino
@@ -9,24 +9,37 @@ void setup()
 
 void loop()
 {
-   char input;
-   
-   if(Serial.available())
+  char input;
+
+  if(Serial.available())
   {
     input = Serial.read();
     Serial.println(input);
   }
-
+  
   if(input == '1')
   {
     Serial.println("Start UP");
     sm.startLiftUp();
-    delay(3000);
+  }
+  else if(input == '2')
+  {
     Serial.println("Start DOWN");
     sm.startLiftDown();
-    
-    input = '0';
   }
-
+  else if(input == '3')
+  {
+    Serial.println("0 Degree");
+    sm.changeDegree(0);
+  }
+  else if(input == '4')
+  {
+    Serial.println("90 Degree");
+    sm.changeDegree(90);
+  }
+  else if(input == '5')
+  {
+    Serial.println("180 Degree");
+    ssm.changeDegree(180);
+  }
 }
-
